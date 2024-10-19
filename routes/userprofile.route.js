5ddde62387bc38676cb15cf9e487c6c3ae69a827
raiszidane95 <raiszidane95@gmail.com>
2024-10-19 14:12:41 +0700

    update

--- routes/userprofile.route.js
+++ routes/userprofile.route.js
@@ -17,8 +17,9 @@ route.get('/user/profile/get', [mid.checkRolesAndLogout(['User'])], userprofileC
 route.put('/user/profile/update/:slug', [mid.checkRolesAndLogout(['User'])], multipleUpload, userprofileController.updateuserprofile);
 route.put('/user/about/update/:slug', [mid.checkRolesAndLogout(['User'])], userprofileController.updateaboutuser);
 route.put('/user/image-profile/update/:slug', [mid.checkRolesAndLogout(['User'])], upload.single('image'), userprofileController.updateImageProfile);
-route.put('/user/cv-portfolio/upload/:slug', [mid.checkRolesAndLogout(['User'])], upload.fields([{ name: 'cv', maxCount: 1 }, { name: 'portfolio', maxCount: 1 }]), userprofileController.uploadDocCvPortfolio); 
-route.get('/user/cv/generate', [mid.checkRolesAndLogout(['User'])], userprofileController.generateCv); 
+route.put('/user/cv-portfolio/upload/:slug', [mid.checkRolesAndLogout(['User'])], upload.fields([{ name: 'cv', maxCount: 1 }, { name: 'portfolio', maxCount: 1 }]), userprofileController.uploadDocCvPortfolio);
+route.get('/user/cv/generate', [mid.checkRolesAndLogout(['User'])], userprofileController.generateCv);
+route.put('/company/profile/update', [mid.checkRolesAndLogout(['Company', 'Super Admin'])], upload.fields([{ name: 'imageLogo', maxCount: 1 }, { name: 'imageBanner', maxCount: 1 }]), userprofileController.updateCompany);
 // route.put('/userprofile/updatefoto/:slug', [mid.checkRolesAndLogout(['Super Admin'])], upload.single('fotoprofil'), userprofileController.updateprofil); 
 
 module.exports = route;
